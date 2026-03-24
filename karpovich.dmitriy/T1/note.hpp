1686a8b620f2806ee120b3bb42641133dcc49694
voyadger01 <voyadgerkx@gmail.com>
2026-03-24 21:47:18 +0300

    decl of class note

---
+++ karpovich.dmitriy/T1/note.hpp
@@ -0,0 +1,30 @@
+#ifndef NOTE_HPP
+#define NOTE_HPP
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <iostream>
+
+namespace karpovich
+{
+  class Note
+  {
+  public:
+    Note(std::string name);
+
+    void addLine(std::string line);
+    void addLink(std::shared_ptr< Note > tolink);
+    void haltLink(const std::string &targetName);
+    void show(std::ostream &out) const;
+    void mindLinks(std::ostream &out) const;
+    void refresh();
+    size_t countExpired() const;
+    const std::string &getName() const;
+  private:
+    std::string name_;
+    std::vector< std::string > lines_;
+    std::vector< std::weak_ptr< Note > > links_;
+  };
+}
+#endif
