6d5204d578810ef847f2d8c23c0ea473dd56c202
voyadger01 <voyadgerkx@gmail.com>
2026-03-25 13:02:53 +0300

    decl of commands

--- karpovich.dmitriy/T1/commands.hpp
+++ karpovich.dmitriy/T1/commands.hpp
@@ -1,9 +1,24 @@
 #ifndef COMMANDS_HPP
 #define COMMANDS_HPP
 
+#include <string>
+#include <unordered_map>
+#include <memory>
+#include "note.hpp"
+
 namespace karpovich
 {
+  using NoteMap = std::unordered_map<std::string, std::shared_ptr<Note>>;
 
+  bool cmd_note(NoteMap& notes, const std::string& name);
+  bool cmd_line(NoteMap& notes, const std::string& name, const std::string& text);
+  bool cmd_show(const NoteMap& notes, const std::string& name);
+  bool cmd_drop(NoteMap& notes, const std::string& name);
+  bool cmd_link(NoteMap& notes, const std::string& from_name, const std::string& to_name);
+  bool cmd_halt(NoteMap& notes, const std::string& from_name, const std::string& to_name);
+  bool cmd_mind(const NoteMap& notes, const std::string& name);
+  bool cmd_expired(const NoteMap& notes, const std::string& name);
+  bool cmd_refresh(NoteMap& notes, const std::string& name);
 }
 
 #endif
