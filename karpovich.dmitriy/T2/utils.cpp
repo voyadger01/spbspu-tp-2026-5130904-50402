709950b823bc771ba46f506f13968459ca7f847d
voyadger01 <voyadgerkx@gmail.com>
2026-05-15 21:36:24 +0300

    add >> for data struct

--- karpovich.dmitriy/T2/utils.cpp
+++ karpovich.dmitriy/T2/utils.cpp
@@ -74,6 +74,51 @@ std::istream &karpovich::operator>>(std::istream &in, StringIO &&dest)
 
 std::istream &karpovich::operator>>(std::istream &in, DataStruct &dest)
 {
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+
+  DataStruct temp{};
+  bool got1 = false;
+  bool got2 = false;
+  bool got3 = false;
+
+  in >> DelimIO{'('};
+  if (!in) {
+    return in;
+  }
+
+  while ((!got1 || !got2 || !got3) && in) {
+    std::string label;
+    in >> label;
+
+    if (label == ":key1" && !got1) {
+      in >> BinIO{temp.key1};
+      if (in) {
+        got1 = true;
+      }
+    } else if (label == ":key2" && !got2) {
+      in >> OctIO{temp.key2};
+      if (in) {
+        got2 = true;
+      }
+    } else if (label == ":key3" && !got3) {
+      in >> StringIO{temp.key3};
+      if (in) {
+        got3 = true;
+      }
+    } else {
+      in.setstate(std::ios::failbit);
+    }
+  }
+  in >> DelimIO{':'} >> DelimIO{')'};
+  if (in && got1 && got2 && got3) {
+    dest = temp;
+  } else {
+    in.setstate(std::ios::failbit);
+  }
+
   return in;
 }
 
