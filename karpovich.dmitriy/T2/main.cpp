2e367632e19c2bad892e92096f1ef170891653f9
voyadger01 <voyadgerkx@gmail.com>
2026-05-15 20:30:35 +0300

    add main

--- karpovich.dmitriy/T2/main.cpp
+++ karpovich.dmitriy/T2/main.cpp
@@ -1,2 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include "utils.hpp"
+
 int main()
-{}
+{
+  using T = karpovich::DataStruct;
+  std::vector< T > data;
+  using iit_t = std::istream_iterator< T >;
+  std::copy(iit_t{std::cin}, iit_t{}, std::back_inserter(data));
+  std::sort(data.begin(), data.end());
+  using oit_t = std::ostream_iterator< T >;
+  std::copy(data.begin(), data.end(), oit_t{std::cout, "\n"});
+}
